6cd441aca4079e89486ac6856bafe81fecdd7ec6
Zaina <Zaina@DESKTOP-EI3LB00>
2020-02-04 07:07:05 -0500

    Working code. Need to edit Word count function. (Temp fix - may not work for all inputs)

--- HW 1/HW 1/Source.cpp
+++ HW 1/HW 1/Source.cpp
@@ -1,6 +1,6 @@
-#include <iostream>
 #include <string>
 #include <fstream>
+#include <iostream>
 
 using namespace std;
 
@@ -30,9 +30,12 @@ Book::Book() {
 
 //this function calculates the number of words there are in the file
 void Book::calculateWordCount(string bookText) {
-	for (int i = 0; i < bookText.size; i++) {
+	if (bookText.length() > 0) {
+		wordCount++;
+	}
+	for (int i = 0; i < bookText.length(); i++) {
 		if (int(bookText[i]) == char(32)) {
-			if (i != bookText.size - 1 && int(bookText[i + 1]) != char(32)){
+			if (i != bookText.length() - 1 && int(bookText[i + 1]) != char(32)){
 				wordCount++;
 			}
 		}
@@ -42,9 +45,9 @@ void Book::calculateWordCount(string bookText) {
 //this function calculates the frequency of each letter in the file
 void Book::calculateLetterFrequency(string bookText) {
 	int index_val;
-	for (int i = 0; i < bookText.size; i++) {
+	for (int i = 0; i < bookText.length(); i++) {
 		index_val = int(tolower(bookText[i])) - 97;
-		if (index_val >= 0 || index_val < 26) {
+		if (index_val >= 0 && index_val < 26) {
 			letterFrequency[index_val]++;
 		}
 	}
@@ -52,20 +55,20 @@ void Book::calculateLetterFrequency(string bookText) {
 
 //this function displays the frequency of each letter in the file as a percentage value
 void Book::displayLetterFrequency() {
-	int lineCount = 0, i;
+	int letterCount = 0, i;
 	for (i = 0; i < 26; i++) {
-		lineCount += letterFrequency[i];
+		letterCount += letterFrequency[i];
 	}
 	for (int i = 0; i < 26; i++) {
-		cout << char(i + 97) << ": " << letterFrequency[i] / letterCount << "%" << endl;
+		cout << char(i + 97) << ": " << double(letterFrequency[i]) * 100 / double(letterCount) << "%" << endl;
 	}
 }
 
+/*
+Detrmines whether the user wants to quit the program
+Retruns boolean true if the user wants to quit and false if the user does not want to quit
+*/
 bool getQuitFlag() {
-	/*
-	Detrmines whether the user wants to quit the program
-	Retruns boolean true if the user wants to quit and false if the user does not want to quit
-	*/
 	char quitChar = '*';
 	cout << "Do you wish to process another book?(y/n) " << endl;
 	cin >> quitChar;
@@ -90,76 +93,89 @@ void saveBook(Book book) {
 	string firstName = book.author.substr(0, book.author.find(" "));
 	string lastName = book.author.substr(book.author.rfind(" ") + 1, book.author.size());
 	outFile << "Author First Name: " + firstName + "\n";
-	outFile << "Author Last Name: " + firstName + "\n";
+	outFile << "Author Last Name: " + lastName + "\n";
 	outFile << "Word count: " + to_string(book.wordCount) + "\n";
 	outFile << "Line Count: " + to_string(book.lineCount) + "\n";
 	outFile << "\n";
 	outFile.close();
 }
 
-void findTitleAndAuthor(Book book_OBJ, ifstream bookFile) {
-	bookFile.seekg(0, ios::beg);
-	getline(bookFile, book_OBJ.title);
-	getline(bookFile, book_OBJ.author);
-}
-
 int main() {
 
 	string fileName;
-	string bookString;
-	string letterFreqRet;
+	string contentsString;
+	string seeFrequencyflag;
 	string lineString;
 	int length = 0;
-	int lineCount = 0;
-	ifstream bookFile;
+	bool contentsFlag = false;
 
 	do {
-		cout << "Provide file name for processing: " << endl;
+
+		cout << "Provide file name for processing: ";
 		cin >> fileName;
+		cout << endl;
+
+		//Creating ifstream object
+		ifstream bookFile;
 
 		//Checking for a valid input File name
 		while (true) {
-			try {
-				bookFile.open(fileName, ios::in);
+			bookFile.open(fileName, ios::in);
+			if (bookFile.is_open()){
 				break;
-			}
-			catch(exception e) {
+			}else{
 				cout << "Please enter a valid file name: ";
 				cin >> fileName;
 				cout << endl;
 			}
 		}
-		// create book object
-		Book book_OBJ;
+
+		//Create book object
+		Book bookObj;
 
 		// The while loop is reading line by line and adding each line to a string that contains the entire text file.
-		
-		while (!bookFile.eof) {
+		while (!bookFile.eof()) {
 			getline(bookFile, lineString);
-			book_OBJ.lineCount++;
-			bookString += " " + lineString;
+			if (bookObj.title == "undefined" && lineString.length() > 0) {
+			//Finding title 
+			bookObj.title = lineString;
+			}
+			else if (bookObj.author == "undefined" && lineString.length() > 0) {
+				//Finding author
+				bookObj.author = lineString;
+			}
+			else if (contentsFlag) {
+				//Finding Line Count (in contents section)
+				bookObj.lineCount++;
+				//Finding Letter Frequency (in contents section)
+				bookObj.calculateLetterFrequency(lineString);
+				//Finding Word Count (in contents section)
+				bookObj.calculateWordCount(lineString);
+			}
+			else if (lineString.find("Contents:") != string::npos) {
+				contentsFlag = true;
+			}
+			else {
+				cout << "Blank line before contents section (omitted for calculation) " << lineString << endl;
+			}
 		}
 
-		//Finding title and author
-		findTitleAndAuthor(book_OBJ, bookFile);
-		//Finding Word Count
-		book_OBJ.calculateWordCount(bookString);
-		//Finding Line Count 
-		book_OBJ.lineCount = lineCount;
-		//Finding Letter Frequency
-		book_OBJ.calculateLetterFrequency(bookString);
 		//Saving book to CardCatalog.txt
-		saveBook(book_OBJ);
+		saveBook(bookObj);
 
-		cout << "Do you wish to see the letter frequency (yes/no): ";
-		cin >> letterFreqRet;
+		cout << "Do you wish to see the letter frequency (y/n): ";
+		cin >> seeFrequencyflag;
 		cout << endl;
-		if (letterFreqRet == "yes") {
-			book_OBJ.displayLetterFrequency();
+		while (seeFrequencyflag != "y" && seeFrequencyflag != "n") {
+			cout << "ERROR: Incorrect selection. Press y for yes/n for no: ";
+			cin >> seeFrequencyflag;
+			cout << endl;
 		}
-		//get quit flag will take care of processing another book
+		if (seeFrequencyflag == "y") {
+			bookObj.displayLetterFrequency();
+		}
+	
+		bookFile.close();
 	} while (!getQuitFlag());
-
-	bookFile.close();
 
 }
